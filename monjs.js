dc0188ac94d3e754df2e58807f699516e484ad0e
JohnFuglu <43675834+JohnFuglu@users.noreply.github.com>
2022-12-25 22:01:50 +0100

    Portable files

---
+++ monjs.js
@@ -0,0 +1,7 @@
+let txt= document.getElementById('ajout');
+let button= document.getElementById('bouton');
+
+button.addEventListener('click',changeTxt);
+function changeTxt(){
+  txt.textContent ='huuuuu dada !';
+}
